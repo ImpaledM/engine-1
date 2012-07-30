e1f30836ea32db13d13efb90fbd49208812bf9e2
Roman <oksamitnyj@gmail.com>
2012-07-30 16:50:35 +0300

    печать

--- modules/gallery/gallery.php
+++ modules/gallery/gallery.php
@@ -9,7 +9,7 @@ class gallery extends Module {
 
 	function add($id = false) {
 		if (! is_null ( parent::add ( $id ) ))
-		XML::from_db ( '//edit/item', 'SELECT `name`, `note` FROM `' . $this->table . '_file` WHERE id_parent=? AND field=?', array ($id, 'photo' ), 'photo' );
+			XML::from_db ( '//edit/item', 'SELECT `name`, `note` FROM `' . $this->table . '_file` WHERE id_parent=? AND field=?', array ($id, 'photo' ), 'photo' );
 	}
 
 	function item($id) {
@@ -18,9 +18,16 @@ class gallery extends Module {
 	}
 
 	function get_list() {
-		$query='SELECT * FROM `' . $this->table . '`';
-		if (!isset($_GET['ADMIN'])) $query.=' WHERE active=1';
-		parent::get_list($query);
+		if (isset($_GET['ADMIN'])) {
+			parent::get_list('SELECT * FROM `' . $this->table . '` WHERE id_section=?', $_GET['section']);
+		} else {
+			$ar=$this->db->get_all('SELECT * FROM `' . $this->table . '` WHERE id_section=? AND active=1', $_GET['section']);
+			if (count($ar)==1) {
+				$this->item($ar[0]['id']);
+			} else {
+				XML::from_array('/', array('list'=>$ar));
+			}
+		}
 	}
 
 }
